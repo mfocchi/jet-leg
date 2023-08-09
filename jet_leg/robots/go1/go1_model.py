e6795100b268a1f40082b4a8fe28c76cb4c76ad1
froscia <francesco.roscia@iit.it>
2023-08-09 08:59:17 +0000

    go1 resources

--- jet_leg/robots/go1/go1_model.py
+++ jet_leg/robots/go1/go1_model.py
@@ -3,17 +3,17 @@ import numpy as np
 class Go1Model:
     def __init__(self):
 
-        self.trunkMass = 12
+        self.trunkMass = 13.081
 
         ''' torque limits for each leg (this code assumes a hyq-like design, i.e. three joints per leg)
         HAA = Hip Abduction Adduction
         HFE = Hip Flextion Extension
         KFE = Knee Flextion Extension
         '''
-        LF_tau_lim = [44.0, 44.0, 55.0]  # HAA, HFE, KFE
-        RF_tau_lim = [44.0, 44.0, 55.0]  # HAA, HFE, KFE
-        LH_tau_lim = [44.0, 44.0, 55.0]  # HAA, HFE, KFE
-        RH_tau_lim = [44.0, 44.0, 55.0]  # HAA, HFE, KFE
+        LF_tau_lim = [23.7 , 23.7 , 35.55]  # HAA, HFE, KFE
+        RF_tau_lim = [23.7 , 23.7 , 35.55]  # HAA, HFE, KFE
+        LH_tau_lim = [23.7 , 23.7 , 35.55]  # HAA, HFE, KFE
+        RH_tau_lim = [23.7 , 23.7 , 35.55]  # HAA, HFE, KFE
 
         self.joint_torque_limits = np.array([LF_tau_lim, RF_tau_lim, LH_tau_lim, RH_tau_lim])
         self.contact_torque_limits = np.array([-1, 1])
