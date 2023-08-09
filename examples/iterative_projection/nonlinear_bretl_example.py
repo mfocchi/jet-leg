79ea940545cf7fd6f40853fe0164bc2003e60732
froscia <francesco.roscia@iit.it>
2023-08-09 08:33:10 +0000

    bugfix: import of Math and IterativeProjectionParameters

--- examples/iterative_projection/nonlinear_bretl_example.py
+++ examples/iterative_projection/nonlinear_bretl_example.py
@@ -9,9 +9,9 @@ import numpy as np
 
 from jet_leg.plotting.plotting_tools import Plotter
 import random
-from jet_leg.maths.math_tools import Math
 from jet_leg.kinematics import kinematics_interface
-from jet_leg.maths.iterative_projection_parameters import IterativeProjectionParameters
+from jet_leg.computational_geometry.math_tools import Math
+from jet_leg.computational_geometry.iterative_projection_parameters import IterativeProjectionParameters
 from jet_leg.optimization import nonlinear_projection
 from shapely.geometry import Polygon
 from jet_leg.dynamics.computational_dynamics import ComputationalDynamics
